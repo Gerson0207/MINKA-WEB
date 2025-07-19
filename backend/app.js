e5951047e552db8d84f837826614766ca95fe4d9
Gerson0207 <gersonportugal60@gmail.com>
2025-07-18 21:44:37 -0400

    modificacion de archivos implementando codigo backend y fronted

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => res.send('API MINKA-WEB funcionando'));
 
-app.listen(3000, () => console.log('Servidor backend escuchando en puerto 3000'));
+const productos = [
+  { id: 1, nombre: 'Celular Samsung', precio: 1500 },
+  { id: 2, nombre: 'Laptop HP', precio: 3500 },
+];
+
+// Ruta simple
+app.get('/', (req, res) => res.send('API MINKA funcionando'));
+
+// Ruta productos
+app.get('/api/productos', (req, res) => res.json(productos));
+
+app.listen(3000, () => console.log('Servidor backend en puerto 3000'));
