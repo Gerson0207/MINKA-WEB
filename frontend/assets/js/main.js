45311111741ef3507d887ad3af0d617dbfdb5ece
Gerson0207 <gersonportugal60@gmail.com>
2025-07-18 22:04:45 -0400

    index y estilo en pagian web

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,10 +1,19 @@
+// Consumir la API y mostrar productos en el index.html
 fetch('http://localhost:3000/api/productos')
   .then(res => res.json())
-  .then(data => {
-    const lista = document.getElementById('lista-productos');
-    data.forEach(producto => {
-      const item = document.createElement('li');
-      item.textContent = `${producto.nombre} - Bs.${producto.precio}`;
-      lista.appendChild(item);
+  .then(productos => {
+    const contenedor = document.getElementById('productos-container');
+
+    productos.forEach(producto => {
+      const div = document.createElement('div');
+      div.classList.add('producto');
+      div.innerHTML = `
+        <h2>${producto.nombre}</h2>
+        <p>Precio: Bs. ${producto.precio}</p>
+      `;
+      contenedor.appendChild(div);
     });
+  })
+  .catch(error => {
+    console.error('Error al cargar los productos:', error);
   });
