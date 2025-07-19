4543c0d5c0cde37f9684a967aa5b36050db3e9dd
Gerson0207 <gersonportugal60@gmail.com>
2025-07-18 21:49:48 -0400

    modificaciones

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,1 +1,10 @@
-// JS principal
+fetch('http://localhost:3000/api/productos')
+  .then(res => res.json())
+  .then(data => {
+    const lista = document.getElementById('lista-productos');
+    data.forEach(producto => {
+      const item = document.createElement('li');
+      item.textContent = `${producto.nombre} - Bs.${producto.precio}`;
+      lista.appendChild(item);
+    });
+  });
